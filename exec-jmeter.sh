9256c85e1915515715e2335ead858a6569be246d
chakradhar <9chakri@gmail.com>
2019-06-13 21:07:46 +0800

    update

--- exec-jmeter.sh
+++ exec-jmeter.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-COUNT=${1-1}
-
+docker-compose stop && docker-compose rm -f
 docker build -t jmeter-base jmeter-base
 docker-compose build && docker-compose up -d && docker-compose scale master=1 slave=$COUNT
 
